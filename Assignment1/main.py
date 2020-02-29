e546f63e4b5f28364d6af7d902dc6b07b2a8581b
JulianRomeroSD <61603388+JulianRomeroSD@users.noreply.github.com>
2020-02-29 04:55:45 -0500

    Hodgkin huxley
    So i coded the hodgkin_huxley. Im pretty sure this is how you do it. Took formulas straight from the paper. So for the part of calculating potentials if you use a timed plot you just would multiply the change in time at each potential for each step. Didnt know how you coded it for yours so just left it there.

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,13 +1,24 @@
 import time
 import random
 import matplotlib.pyplot as plt
+import math
 
 
 # Constants
-CAPACITANCE = 10
+CAPACITANCE = 1
 RESISTANCE = 25
 THRESHOLD = 2
+#Constants for Hoxley
 
+VoltageNa = -115
+VoltageK = 12
+VoltageL = -10.613
+
+maximumConductanceOfNa = 120
+maximumConductanceOfK = 36
+maximumConductanceOfL = 0.3
+
+#izhikevich
 A = 0.02
 B = 0.02
 C = -65 #mV
@@ -19,6 +30,7 @@ recovery = 0
 n = 0
 m = 0
 h = 0
+#
 
 def LIF(input, potential, output):
     # LIF formula
@@ -34,6 +46,10 @@ def LIF(input, potential, output):
 def izhikevich(input, potential, recovery, output):
     # Izhikevich formula
     v = potential
+
+
+    print("v", ":", v)
+
     potential += 0.04 * v ** 2 + 5 * v + 140 - recovery + input
     recovery += A * (B * v - recovery)
 
@@ -47,16 +63,58 @@ def izhikevich(input, potential, recovery, output):
     output[1].append(recovery)
     return potential, recovery
 
-def hodgkin_huxley(input, potential, n, m, h, output):
-    potential += 0
-    n += 0
-    m += 0
-    h += 0
+
+
+def hodgkin_huxley(input, potential, n , m , h, output):
+    v = potential
+
+    #print ("v :", v)
+
+    alpha_n = 0.01 * ((10 + v)/(math.exp((10 + v) / 10)-1))
+    beta_n = 0.125 * math.exp(v/80)
+
+    alpha_m = 0.1 * ((25 + v)/(math.exp((25 + v)/10) - 1))
+    beta_m = 4 * math.exp(v/18)
+
+    alpha_h = 0.07 * math.exp(v/20)
+    beta_h = 1 / (math.exp((30 + v)/10) + 1)
+
+    n = alpha_n/(alpha_n+beta_n)
+    #print ("n : ", n)
+    #proportion of ion channels in which the activation gate is open
+    m = alpha_m/(alpha_m+beta_m)
+    #print ("m : ", m)
+    #proportion of ion channels in which the activation gate is open
+    h = alpha_h/(alpha_h+beta_h)
+    #print ("h : ", h)
+    #sodium ion channels have both activation and inactivation gates
+    ionConductanceOfNa = maximumConductanceOfNa * m * h
+    #potassium ion channels have only activation gates
+    ionConductanceOfK = maximumConductanceOfK * m
+    #Leaky ion channels have only activation gates
+    ionConductanceOfL = maximumConductanceOfL
+
+
+    #currents
+    sodium = ionConductanceOfNa * (m**3) * h * (v - VoltageNa)
+    potassium = ionConductanceOfK * (n**4) * (v - VoltageK)
+    leaky = ionConductanceOfL * (v - VoltageL)
+
+    #potentials
+    potential += (input - sodium - potassium - leaky) / CAPACITANCE
+    n +=  alpha_n * (1-n) - beta_n * n
+    m +=  alpha_m * (1-m) - beta_m * m
+    h +=  alpha_h * (1-h) - beta_h * h
+
+
+
 
     output[0].append(potential)
+    potential = 0
     output[1].append(n)
     output[2].append(m)
     output[3].append(h)
+
 
     return potential, n, m, h
 
@@ -67,26 +125,32 @@ input = [
     (50, 1),
     (100, 0),
     (150, 1),
-    (200, 0)
+    (200, 0),
+
+
+
 ]
 output = [[], [], [], []]
 
 tick = 0
 while len(input) > 1:
     # Handle timed input system
+
     if tick >= input[1][0]:
         input.pop(0)
     tick += 1
 
     i = 111 if random.random() < input[0][1] else 0
 
-    # potential = LIF(i, potential, output)
-    # potential, recovery = izhikevich(i, potential, recovery, output)
+
+
+    #potential = LIF(i, potential, output)
+    #potential, recovery = izhikevich(i, potential, recovery, output)
     potential, n, m, h = hodgkin_huxley(i, potential, n, m, h, output)
 
 plt.plot(output[0], label="potential")
-plt.plot(output[1], label="recovery")
-plt.plot(output[2])
-plt.plot(output[3])
+plt.plot(output[1], label="n")
+plt.plot(output[2], label="m")
+plt.plot(output[3], label="h")
 plt.legend()
 plt.show()
