d0852ec1360266f0012218329ce11976b20260ab
Ryan Cirincione <ryancirincione@aol.com>
2020-04-03 13:04:46 -0400

    Last minute stuff for graphs

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,12 +1,22 @@
 import time
 import random
+import math
 import matplotlib.pyplot as plt
 
 
 # Constants
-CAPACITANCE = 10
-RESISTANCE = 25
-THRESHOLD = 2
+CAPACITANCE = 8
+RESISTANCE = 2
+THRESHOLD = 5
+RESET = 0
+
+VoltageNa = -115
+VoltageK = 12
+VoltageL = -10.613
+
+maximumConductanceOfNa = 120
+maximumConductanceOfK = 36
+maximumConductanceOfL = 0.3
 
 A = 0.02
 B = 0.02
@@ -23,13 +33,15 @@ h = 0
 def LIF(input, potential, output):
     # LIF formula
     potential += (input - potential/RESISTANCE) / CAPACITANCE
+    spike = 0
 
     # If membrane potential reaches threshold, fire!
     if potential > THRESHOLD:
-        potential = 0
+        spike = 1
+        potential = RESET
 
     output[0].append(potential)
-    return potential
+    return potential, spike
 
 def izhikevich(input, potential, recovery, output):
     # Izhikevich formula
@@ -48,12 +60,38 @@ def izhikevich(input, potential, recovery, output):
     return potential, recovery
 
 def hodgkin_huxley(input, potential, n, m, h, output):
-    potential += 0
-    n += 0
-    m += 0
-    h += 0
+    v = potential
+
+    alpha_n = 0.01 * ((10 + v) / (math.exp((10 + v) / 10) - 1))
+    beta_n = 0.125 * math.exp(v / 80)
+
+    alpha_m = 0.1 * ((25 + v) / (math.exp((25 + v) / 10) - 1))
+    beta_m = 4 * math.exp(v / 18)
+
+    alpha_h = 0.07 * math.exp(v / 20)
+    beta_h = 1 / (math.exp((30 + v) / 10) + 1)
+
+    n = alpha_n / (alpha_n + beta_n)
+    m = alpha_m / (alpha_m + beta_m)
+    h = alpha_h / (alpha_h + beta_h)
+
+    ionConductanceOfNa = maximumConductanceOfNa * m * h
+    ionConductanceOfK = maximumConductanceOfK * m
+    ionConductanceOfL = maximumConductanceOfL
+
+    sodium = ionConductanceOfNa * (m**3) * h * (v - VoltageNa)
+    potassium = ionConductanceOfK * (n**4) * (v - VoltageK)
+    leaky = ionConductanceOfL * (v - VoltageL)
+
+    potential += (input - sodium - potassium - leaky) / CAPACITANCE
+    n += 0.01 * (alpha_n * (1-n) - beta_n * n)
+    m += 0.01 * (alpha_m * (1-m) - beta_m * m)
+    h += 0.01 * (alpha_h * (1-h) - beta_h * h)
 
     output[0].append(potential)
+    # potential = 0
+    if potential > 1:
+        potential = -7
     output[1].append(n)
     output[2].append(m)
     output[3].append(h)
@@ -64,29 +102,41 @@ def hodgkin_huxley(input, potential, n, m, h, output):
 # At time x, change to input probability y
 input = [
     (0, 0),
-    (50, 1),
-    (100, 0),
-    (150, 1),
-    (200, 0)
+    (250, 1),
+    (500, 0),
+    (750, 1),
+    (1000, 0)
 ]
 output = [[], [], [], []]
+spikeGraph = []
 
 tick = 0
+spikes = 0
 while len(input) > 1:
     # Handle timed input system
     if tick >= input[1][0]:
         input.pop(0)
+        # spikeGraph.append(spikes)
+        # spikes = 0
     tick += 1
 
-    i = 111 if random.random() < input[0][1] else 0
 
-    # potential = LIF(i, potential, output)
+    i = 5 if random.random() < input[0][1] else 0
+    # i = input[0][1]
+
+    # potential, s = LIF(i, potential, output)
     # potential, recovery = izhikevich(i, potential, recovery, output)
     potential, n, m, h = hodgkin_huxley(i, potential, n, m, h, output)
 
+    # spikes += s
+
+# plt.plot([0,1,2,3,4,5,6,7,8,9,10,11], spikeGraph)
 plt.plot(output[0], label="potential")
-plt.plot(output[1], label="recovery")
-plt.plot(output[2])
-plt.plot(output[3])
+plt.plot(output[1], label="n")
+plt.plot(output[2], label="m")
+plt.plot(output[3], label="h")
+plt.xlabel("time")
+# plt.ylabel("spikes per 50 ms")
+# plt.xlabel("input")
 plt.legend()
 plt.show()
