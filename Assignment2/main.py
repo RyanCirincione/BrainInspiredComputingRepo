60309e58c0c39085a059ff351a8d2fb4a979c243
Ryan Cirincione <ryancirincione@aol.com>
2020-04-26 17:28:41 -0400

    Added network function, and made several changes so that neurons now simulate their input weights

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -1,24 +1,39 @@
 CAPACITANCE = 2
 RESISTANCE = 2
 THRESHOLD = 15
+SPIKE_VALUE = 1
 
 class LIFNeuron:
     potential = 0
+    weights = []
 
-    def update(self, input):
-        potential += (input - potential/RESISTANCE) / CAPACITANCE
+    def __init__(self, numInputs):
+        self.weights = [0.5] * numInputs # TODO: change 0.5 to be random, otherwise network is symmetric
+
+    def update(self, inputs):
+        for i in range(0, len(inputs)):
+            inputs[i] = inputs[i] * self.weights[i]
+
+        for input in inputs:
+            potential += (input - potential/RESISTANCE) / CAPACITANCE
         # If membrane potential reaches threshold, fire!
         if potential > THRESHOLD:
-            return True
+            return SPIKE_VALUE
             potential = 0
 
-        return False
+        return 0
 
 HIDDEN_LAYER_SIZE = 5
 
-x = LIFNeuron()
-y = LIFNeuron()
-out = LIFNeuron()
+outNeuron = LIFNeuron(HIDDEN_LAYER_SIZE)
 hiddenLayer = []
 for i in range(0, HIDDEN_LAYER_SIZE):
-    hiddenLayer.append(LIFNeuron())
+    hiddenLayer.append(LIFNeuron(2))
+
+# x and y are simulated input neurons
+def updateNetwork(x, y, hiddenLayer, out):
+    spikes = []
+    for i in range(0, len(hiddenLayer)):
+        spikes.append(hiddenLayer[i].update([x, y]))
+
+    outNeuron.update(spikes)
