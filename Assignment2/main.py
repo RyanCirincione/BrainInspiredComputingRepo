d248c1299244a853d9e0655033e7497421e94d51
Ryan Cirincione <ryancirincione@aol.com>
2020-04-26 02:53:48 -0400

    Added basic LIF definition and structure for Q3

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -1,1 +1,21 @@
-# Assignment 2
+CAPACITANCE = 2
+RESISTANCE = 2
+THRESHOLD = 15
+
+class LIFNeuron:
+    potential = 0
+
+    def update(self, input):
+        potential += (input - potential/RESISTANCE) / CAPACITANCE
+        # If membrane potential reaches threshold, fire!
+        if potential > THRESHOLD:
+            potential = 0
+
+HIDDEN_LAYER_SIZE = 5
+
+x = LIFNeuron()
+y = LIFNeuron()
+out = LIFNeuron()
+hiddenLayer = []
+for i in range(0, HIDDEN_LAYER_SIZE):
+    hiddenLayer.append(LIFNeuron())
